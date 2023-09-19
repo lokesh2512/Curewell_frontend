1be283c7b2c70bc69c579880d81a1921e1381066
lokesh2512 <lokeshgupta2512@gmail.com>
2023-09-19 17:38:31 +0530

    added forntend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NewHomeComponent } from './new-home/new-home.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NewHomeComponent,
+
+    
+
   ],
   imports: [
     BrowserModule,
