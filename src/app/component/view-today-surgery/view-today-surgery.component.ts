ade35853556b24287e8f48c079cdeb6a3eb6f6bf
PandeyHariOm07 <05altamash05@gmail.com>
2023-09-20 16:51:55 +0530

    components added

---
+++ src/app/component/view-today-surgery/view-today-surgery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-today-surgery',
+  templateUrl: './view-today-surgery.component.html',
+  styleUrls: ['./view-today-surgery.component.scss']
+})
+export class ViewTodaySurgeryComponent {
+
+}
